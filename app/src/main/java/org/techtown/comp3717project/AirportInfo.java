78a38d40e92a08f358a1836aae529cb400d30df5
RaphaelYun <raphael.yun@gmail.com>
2022-10-05 13:17:42 -0700

    initialized airport info page

---
+++ app/src/main/java/org/techtown/comp3717project/AirportInfo.java
@@ -0,0 +1,14 @@
+package org.techtown.comp3717project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AirportInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_airport_info);
+    }
+}
