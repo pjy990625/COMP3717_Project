4182801de9679abea025172e8b363e0d12fd29f8
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-10-10 10:27:37 -0700

    Added fragment generation

--- app/src/main/java/org/techtown/comp3717_project/ServiceActivity.java
+++ app/src/main/java/org/techtown/comp3717_project/ServiceActivity.java
@@ -1,6 +1,8 @@
 package org.techtown.comp3717_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,9 @@ public class ServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+        search search = new search();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.nav_host_fragment_activity_service, search).commit();
     }
 }
