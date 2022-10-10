e76e16273ac5a6518f803582855afdecf2031512
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-10-10 14:46:47 -0700

    Added new variables

--- app/src/main/java/org/techtown/comp3717_project/Airport.java
+++ app/src/main/java/org/techtown/comp3717_project/Airport.java
@@ -3,6 +3,7 @@ package org.techtown.comp3717_project;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Airport extends Fragment {
+    RecyclerView items, services;
+    String[] itemNames, serviceNames;
+    int[] dutyFreeIcons = {R.drawable.ic_baseline_smoking_rooms_24,
+            R.drawable.ic_baseline_local_drink_24, R.drawable.ic_baseline_shopping_cart_24};
+    int[] servicesIcons = {R.drawable.ic_baseline_car_rental_24, R.drawable.ic_baseline_hotel_24,
+    R.drawable.ic_baseline_local_taxi_24};
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
