098f0ebe19be2c1207e1925b4361e84e717bc4a7
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-10-11 14:48:49 -0700

    Renamed Info

--- app/src/main/java/org/techtown/comp3717_project/InfoActivity.java
+++ app/src/main/java/org/techtown/comp3717_project/InfoActivity.java
@@ -3,12 +3,14 @@ package org.techtown.comp3717_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class Info extends AppCompatActivity {
+public class InfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        TextView name = findViewById(R.id.textViewAirportName);
     }
 }
