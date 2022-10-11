3aa38fa49884d0a691928020a6480cb30eec2b8d
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-10-11 14:43:35 -0700

    Deleted home

--- app/src/main/java/org/techtown/comp3717_project/MainActivity.java
+++ app/src/main/java/org/techtown/comp3717_project/MainActivity.java
@@ -34,10 +34,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-        Home home = new Home();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.nav_host_fragment_activity_main, home).commit();
     }
 
 }
--- app/src/main/java/org/techtown/comp3717_project/ui/home/HomeFragment.java
+++ app/src/main/java/org/techtown/comp3717_project/ui/home/HomeFragment.java
@@ -1,15 +1,19 @@
 package org.techtown.comp3717_project.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.techtown.comp3717_project.R;
+import org.techtown.comp3717_project.ServiceActivity;
 import org.techtown.comp3717_project.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -26,6 +30,11 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.welcome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Button services = root.findViewById(R.id.service);
+        services.setOnClickListener((view1 -> {
+            Intent intent = new Intent(this.getContext(), ServiceActivity.class);
+            startActivity(intent);
+        }));
         return root;
     }
 
