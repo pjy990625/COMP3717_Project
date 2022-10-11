77ce7cfc8d6c03df68247ababb4eb3ef75909d16
Skylar Park <pjy990625@gmail.com>
2022-10-11 13:57:22 -0700

    fixed errors

---
+++ app/src/main/java/org/techtown/comp3717_project/CompareActivity.java
@@ -0,0 +1,14 @@
+package org.techtown.comp3717_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CompareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compare);
+    }
+}
