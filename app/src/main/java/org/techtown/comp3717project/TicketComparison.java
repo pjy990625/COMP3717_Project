3335d9905a917dd42a18dd6e4d36b6710dcf7d4f
Sarah Dong <sarahdn13@icloud.com>
2022-10-12 09:40:10 -0700

    ticket info activity

---
+++ app/src/main/java/org/techtown/comp3717project/TicketComparison.java
@@ -0,0 +1,14 @@
+package org.techtown.comp3717project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TicketComparison extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_comparison);
+    }
+}
---
+++ app/src/main/java/org/techtown/comp3717project/TicketInfo.java
@@ -0,0 +1,14 @@
+package org.techtown.comp3717project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TicketInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_info);
+    }
+}
